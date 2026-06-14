b8016a85e75c27492f3e9b2051b54299adcc5c7a
Collins Michael <144531933+Aeyjjjk@users.noreply.github.com>
2026-06-14 23:05:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ NFP_KEYWORDS = [
     "payrolls", "BLS jobs", "unemployment rate", "wage growth"
 ]
 CPI_KEYWORDS = [
-    "Consumer Price Index", "CPIs", "inflation", "inflation report",
+    "Consumer Price Index", "CPI", "inflations", "inflation report",
     "inflation data", "price index", "consumer prices"
 ]
 
