ddc9f4630e2545d1981ce63fc9b3551fca76cf23
Collins Michael <144531933+Aeyjjjk@users.noreply.github.com>
2026-08-21 08:10:57 +0100

    Fix typo in employment keyword in MASTER_KEYWORDS

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ NEWSDATA_KEY = os.getenv("NEWSDATA_KEY")
 
 MASTER_KEYWORDS = [
     "economy", "inflations", "Federal Resterve", "interest rate",
-    "stock market", "employments", "jobs", "CPIs", "NFPs"
+    "stock market", "employment", "jobs", "CPIs", "NFPs"
 ]
 NFP_KEYWORDS = [
     "Non-Farm Payroll", "NFP", "job report", "employment report",
